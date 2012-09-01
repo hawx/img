0eb5830622bb9fdc28f206dba374d52c905d9bcb
Joshua Hawxwell <m@hawx.me>
2012-09-01 11:31:36 +0100

    Updated contrast

--- contrast.go
+++ contrast.go
@@ -1,31 +1,12 @@
 package main
 
 import (
+	"./contrast"
 	"./utils"
 	"os"
-	"image"
-	"image/color"
 	"fmt"
 	"flag"
 )
-
-func adjustContrast(img image.Image, value float64) image.Image {
-	value  = (100 + value) / 100
-	value *= value
-
-	f := func(c color.Color) color.Color {
-		r,g,b,a := utils.RatioRGBA(c)
-
-		r = utils.Truncatef((((r - 0.5) * value) + 0.5) * 255)
-		g = utils.Truncatef((((g - 0.5) * value) + 0.5) * 255)
-		b = utils.Truncatef((((b - 0.5) * value) + 0.5) * 255)
-		a = a * 255
-
-		return color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
-	}
-
-	return utils.EachPixel(img, f)
-}
 
 func printHelp() {
 	msg := "Usage: contrast [value]\n" +
@@ -33,6 +14,7 @@ func printHelp() {
 		"  Takes a png file from STDIN, adjusts the contrast using the value given\n" +
 		"  and prints the result to STDOUT.\n" +
 		"\n" +
+		"  --by          # Amount to shift contrast by (default: 15.0)\n" +
 		"  --help        # Display this help message\n" +
 		"\n"
 
@@ -41,7 +23,7 @@ func printHelp() {
 }
 
 var help = flag.Bool("help", false, "Display this help message")
-var by   = flag.Float64("by", 15.0, "Amount to shift hue by")
+var by   = flag.Float64("by", 15.0, "Amount to shift contrast by")
 
 func main() {
 	flag.Parse()
@@ -51,6 +33,6 @@ func main() {
 	}
 
 	i := utils.ReadStdin()
-	i  = adjustContrast(i, *by)
+	i  = contrast.Adjust(i, *by)
 	utils.WriteStdout(i)
 }
