0c8e2575e3952ecd910e5c43efa2a2e6e1ca7c19
Joshua Hawxwell <m@hawx.me>
2012-09-09 16:41:58 +0100

    Translated all blend modes, plus a few more

--- blend.go
+++ blend.go
@@ -27,6 +27,35 @@ func printHelp() {
 		"  --normal       # Paints pixels using <other> (default)\n" +
 		"  --dissolve     # Paints pixels from <other> randomly, depending on opacity\n" +
 		"\n" +
+		"  DARKEN\n" +
+		"  --darken       # Selects the darkest value for each colour channel\n" +
+		"  --multiply     # Multiplies each colour channel\n" +
+		"  --burn         # Darkens the base colour to increase contrast\n" +
+		"  --darker       # Selects the darkest colour by comparing the sum of channels\n" +
+		"\n" +
+		"  LIGHTEN\n" +
+		"  --lighten      # Selects the lightest value for each colour channel\n" +
+		"  --screen       # Multiples the inverse of each colour channel\n" +
+		"  --dodge        # Brightens the base colour to decrease contrast\n" +
+		"  --lighter      # Selects the lightest colour by comparing the sum of channels\n" +
+		"\n" +
+		"  CONTRAST\n" +
+		"  --overlay      # Multiplies or screens the colours, depending on the base colour\n" +
+		"  --soft-light   # Darkens or lightens the colours, depending on the blend colour\n" +
+		"  --hard-light   # Multiplies or screens the colours, depending on the blend colour\n" +
+		"\n" +
+		"  COMPARATIVE\n" +
+		"  --difference   # Finds the absolute difference between the base and blend colour\n" +
+		"  --exclusion    # Creates an effect similar to, but lower in contrast, than difference\n" +
+		"  --addition     # Adds the blend colour to the base colour\n" +
+		"  --subtraction  # Subtracts the blend colour from the base colour\n" +
+		"\n" +
+		"  HSL\n" +
+		"  --hue          # Uses just the hue of the blend colour\n" +
+		"  --saturation   # Uses just the saturation of the blend colour\n" +
+		"  --color        # Uses just the hue and saturation of the blend colour\n" +
+		"  --luminosity   # Uses just the luminosity of the blend colour\n" +
+		"\n" +
 		"  --help         # Display this help message\n" +
 		"\n"
 
@@ -42,6 +71,37 @@ var opacity      = flag.Float64("opacity", 1.0, "")
 var normalM      = flag.Bool("normal", false, "")
 var dissolveM    = flag.Bool("dissolve", false, "")
 
+// DARKEN
+var	darkenM      = flag.Bool("darken", false, "")
+var	multiplyM    = flag.Bool("multiply", false, "")
+var	burnM        = flag.Bool("burn", false, "")
+var darkerM      = flag.Bool("darker", false, "")
+
+// LIGHTEN
+var	lightenM     = flag.Bool("lighten", false, "")
+var	screenM      = flag.Bool("screen", false, "")
+var	dodgeM       = flag.Bool("dodge", false, "")
+var lighterM     = flag.Bool("lighter", false, "")
+
+// CONTRAST
+var	overlayM     = flag.Bool("overlay", false, "")
+var	softLightM   = flag.Bool("soft-light", false, "")
+var	hardLightM   = flag.Bool("hard-light", false, "")
+
+// COMPARATIVE
+var	differenceM  = flag.Bool("difference", false, "")
+var exclusionM   = flag.Bool("exclusion", false, "")
+var	additionM    = flag.Bool("addition", false, "")
+var	subtractionM = flag.Bool("subtraction", false, "")
+
+// HSL
+var	hueM         = flag.Bool("hue", false, "")
+var	saturationM  = flag.Bool("saturation", false, "")
+var	colorM       = flag.Bool("color", false, "")
+var	luminosityM  = flag.Bool("luminosity", false, "")
+
+
+
 func main() {
 	flag.Parse()
 	if *help { printHelp() }
@@ -49,17 +109,63 @@ func main() {
 	a := utils.ReadStdin()
 
 	path := flag.Args()[0]
-	f, _ := os.Open(path)
-	b, _ := png.Decode(f)
-	var img image.Image
+	file, _ := os.Open(path)
+	b, _ := png.Decode(file)
+	var f (func(a, b image.Image) image.Image)
 
 	b = blend.Fade(b, *opacity)
 
 	if *normalM {
-		img = blend.Normal(a, b)
+		f = blend.Normal
 	} else if *dissolveM {
-		img = blend.Dissolve(a, b)
+		f = blend.Dissolve
+
+	} else if *darkenM {
+		f = blend.Darken
+	} else if *multiplyM {
+		f = blend.Multiply
+	} else if *burnM {
+		f = blend.Burn
+	} else if *darkerM {
+		f = blend.Darker
+
+	} else if *lightenM {
+		f = blend.Lighten
+	} else if *screenM {
+		f = blend.Screen
+	} else if *dodgeM {
+		f = blend.Dodge
+	} else if *lighterM {
+		f = blend.Lighter
+
+	} else if *overlayM {
+		f = blend.Overlay
+	} else if *softLightM {
+		f = blend.SoftLight
+	} else if *hardLightM {
+		f = blend.HardLight
+
+	} else if *differenceM {
+		f = blend.Difference
+	} else if *exclusionM {
+		f = blend.Exclusion
+	} else if *additionM {
+		f = blend.Addition
+	} else if *subtractionM {
+		f = blend.Subtraction
+
+	} else if *hueM {
+		f = blend.Hue
+	} else if *saturationM {
+		f = blend.Saturation
+	} else if *colorM {
+		f = blend.Color
+	} else if *luminosityM {
+		f = blend.Luminosity
+
+	} else {
+		f = blend.Normal
 	}
 
-	utils.WriteStdout(img)
+	utils.WriteStdout(f(a, b))
 }
--- blend/blend.go
+++ blend/blend.go
@@ -2,6 +2,8 @@ package blend
 
 import (
 	"../utils"
+	"../hsl"
+	"math"
 	"math/rand"
 	"image"
 	"image/color"
@@ -78,29 +80,253 @@ func ratioNRGBA(r, g, b, a float64) color.Color {
 
 
 func normal(c, d color.Color) color.Color {
+	return d
+}
+
+func dissolve(c, d color.Color) color.Color {
+	if r,g,b,a := utils.RatioRGBA(d); rand.Float64() < a {
+		return ratioNRGBA(r, g, b, 1)
+	}
+	return color.Transparent
+}
+
+func darken(c, d color.Color) color.Color {
 	i, j, k, l := utils.RatioRGBA(c)
 	m, n, o, p := utils.RatioRGBA(d)
 
-	r := i * (l - p) + m * p
-	g := j * (l - p) + n * p
-	b := k * (l - p) + o * p
-	a := l + p
+	r := utils.Minf(i, m)
+	g := utils.Minf(j, n)
+	b := utils.Minf(k, o)
+	a := utils.Minf(l, p)
 
 	return ratioNRGBA(r, g, b, a)
 }
 
-func dissolve(c, d color.Color) color.Color {
-	var r, g, b, a float64
+func multiply(c, d color.Color) color.Color {
 	i, j, k, l := utils.RatioRGBA(c)
 	m, n, o, p := utils.RatioRGBA(d)
 
-	if rand.Float64() < p {
-		r = m; g = n; b = o; a = 1
-	} else {
-		r = i; g = j; b = k; a = utils.Maxf(l, p)
-	}
+	r := i * m
+	g := j * n
+	b := k * o
+	a := l * p
 
 	return ratioNRGBA(r, g, b, a)
+}
+
+func burn(c, d color.Color) color.Color {
+	i, j, k, l := utils.RatioRGBA(c)
+	m, n, o, p := utils.RatioRGBA(d)
+
+	r := 1 - ((1 - i) / m)
+	g := 1 - ((1 - j) / n)
+	b := 1 - ((1 - k) / o)
+	a := p + l * (1 - p)
+
+	return ratioNRGBA(r, g, b, a)
+}
+
+func darker(c, d color.Color) color.Color {
+	i, j, k, _ := utils.RatioRGBA(c)
+	m, n, o, _ := utils.RatioRGBA(d)
+
+	if i + j + k < m + n + o {
+		return c
+	}
+	return d
+}
+
+func lighten(c, d color.Color) color.Color {
+	i, j, k, l := utils.RatioRGBA(c)
+	m, n, o, p := utils.RatioRGBA(d)
+
+	r := utils.Maxf(i, m)
+	g := utils.Maxf(j, n)
+	b := utils.Maxf(k, o)
+	a := utils.Maxf(l, p)
+
+	return ratioNRGBA(r, g, b, a)
+}
+
+func screen(c, d color.Color) color.Color {
+	i, j, k, l := utils.RatioRGBA(c)
+	m, n, o, p := utils.RatioRGBA(d)
+
+	r := 1 - ((1 - i) * (1 - m))
+	g := 1 - ((1 - j) * (1 - n))
+	b := 1 - ((1 - k) * (1 - o))
+	a := p + l * (1 - p)
+
+	return ratioNRGBA(r, g, b, a)
+}
+
+func dodge(c, d color.Color) color.Color {
+	i, j, k, l := utils.RatioRGBA(c)
+	m, n, o, p := utils.RatioRGBA(d)
+
+	r := i / (1 - m)
+	g := j / (1 - n)
+	b := k / (1 - o)
+	a := p + l * (1 - p)
+
+	return ratioNRGBA(r, g, b, a)
+}
+
+func lighter(c, d color.Color) color.Color {
+	i, j, k, _ := utils.RatioRGBA(c)
+	m, n, o, _ := utils.RatioRGBA(d)
+
+	if i + j + k > m + n + o {
+		return c
+	}
+	return d
+}
+
+func overlay(c, d color.Color) color.Color {
+	i, j, k, l := utils.NormalisedRGBAf(c)
+	m, n, o, p := utils.NormalisedRGBAf(d)
+
+	r := (i / 255) * (i + ((2 * m) / 255) * (255 - i))
+	g := (j / 255) * (j + ((2 * n) / 255) * (255 - j))
+	b := (k / 255) * (k + ((2 * o) / 255) * (255 - k))
+	a := p + l * (1 - p)
+
+	return color.NRGBA{
+		uint8(utils.Truncatef(r)),
+		uint8(utils.Truncatef(g)),
+		uint8(utils.Truncatef(b)),
+		uint8(utils.Truncatef(a * 255)),
+	}
+}
+
+func softLight(c, d color.Color) color.Color {
+	i, j, k, l := utils.RatioRGBA(c)
+	m, n, o, p := utils.RatioRGBA(d)
+
+	f := func(i, j float64) float64 {
+		if j > 0.5 {
+			return 1 - (1 - i) * (1 - (j - 0.5))
+		}
+		return i * (j + 0.5)
+	}
+
+	r := f(i, m)
+	g := f(j, n)
+	b := f(k, o)
+	a := p + l * (1 - p)
+
+	return ratioNRGBA(r, g, b, a)
+}
+
+func hardLight(c, d color.Color) color.Color {
+	i, j, k, l := utils.NormalisedRGBAf(c)
+	m, n, o, p := utils.NormalisedRGBAf(d)
+
+	f := func(i, j float64) float64 {
+		if j > 128 {
+			return 255 - ((255 - 2 * (j - 128)) * (255 - i)) / 256
+		}
+		return (2 * j * i) / 256
+	}
+
+	r := f(i, m)
+	g := f(j, n)
+	b := f(k, o)
+	a := p + l * (1 - p)
+
+	return color.NRGBA{
+		uint8(utils.Truncatef(r)),
+		uint8(utils.Truncatef(g)),
+		uint8(utils.Truncatef(b)),
+		uint8(utils.Truncatef(a * 255)),
+	}
+}
+
+func difference(c, d color.Color) color.Color {
+	i, j, k, l := utils.RatioRGBA(c)
+	m, n, o, p := utils.RatioRGBA(d)
+
+	r := math.Abs(m - i)
+	g := math.Abs(n - j)
+	b := math.Abs(o - k)
+	a := p + l * (1 - p)
+
+	return ratioNRGBA(r, g, b, a)
+}
+
+func exclusion(c, d color.Color) color.Color {
+	i, j, k, l := utils.RatioRGBA(c)
+	m, n, o, p := utils.RatioRGBA(d)
+
+	r := m + i - (2 * m * i)
+	g := n + j - (2 * n * j)
+	b := o + k - (2 * o * k)
+	a := p + l * (1 - p)
+
+	return ratioNRGBA(r, g, b, a)
+}
+
+// aka Linear Dodge
+func addition(c, d color.Color) color.Color {
+	i, j, k, l := utils.NormalisedRGBA(c)
+	m, n, o, p := utils.NormalisedRGBA(d)
+
+	r := utils.Min(i + m, 255)
+	g := utils.Min(j + n, 255)
+	b := utils.Min(k + o, 255)
+	a := utils.Min(l + p, 255)
+
+	return color.NRGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
+}
+
+func subtraction(c, d color.Color) color.Color {
+	i, j, k, l := utils.NormalisedRGBA(c)
+	m, n, o, p := utils.NormalisedRGBA(d)
+
+	r := utils.Truncate(i - m)
+	g := utils.Truncate(j - n)
+	b := utils.Truncate(k - o)
+
+	if m > i { r = 0 }
+	if n > j { g = 0 }
+	if o > k { b = 0 }
+
+	a := p + l * (1 - p)
+
+	return color.NRGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
+}
+
+func hue(c, d color.Color) color.Color {
+	i := hsl.HSLAModel.Convert(c).(hsl.HSLA)
+	j := hsl.HSLAModel.Convert(d).(hsl.HSLA)
+	i.H = j.H
+
+	return i
+}
+
+func saturation(c, d color.Color) color.Color {
+	i := hsl.HSLAModel.Convert(c).(hsl.HSLA)
+	j := hsl.HSLAModel.Convert(d).(hsl.HSLA)
+	i.S = j.S
+
+	return i
+}
+
+func colour(c, d color.Color) color.Color {
+	i := hsl.HSLAModel.Convert(c).(hsl.HSLA)
+	j := hsl.HSLAModel.Convert(d).(hsl.HSLA)
+	i.H = j.H
+	i.S = j.S
+
+	return i
+}
+
+func luminosity(c, d color.Color) color.Color {
+	i := hsl.HSLAModel.Convert(c).(hsl.HSLA)
+	j := hsl.HSLAModel.Convert(d).(hsl.HSLA)
+	i.L = j.L
+
+	return i
 }
 
 func Normal(a, b image.Image) image.Image {
@@ -109,4 +335,80 @@ func Normal(a, b image.Image) image.Image {
 
 func Dissolve(a, b image.Image) image.Image {
 	return BlendPixels(a, b, dissolve)
+}
+
+func Darken(a, b image.Image) image.Image {
+	return BlendPixels(a, b, darken)
+}
+
+func Multiply(a, b image.Image) image.Image {
+	return BlendPixels(a, b, multiply)
+}
+
+func Burn(a, b image.Image) image.Image {
+	return BlendPixels(a, b, burn)
+}
+
+func Darker(a, b image.Image) image.Image {
+	return BlendPixels(a, b, darker)
+}
+
+func Lighten(a, b image.Image) image.Image {
+	return BlendPixels(a, b, lighten)
+}
+
+func Screen(a, b image.Image) image.Image {
+	return BlendPixels(a, b, screen)
+}
+
+func Dodge(a, b image.Image) image.Image {
+	return BlendPixels(a, b, dodge)
+}
+
+func Lighter(a, b image.Image) image.Image {
+	return BlendPixels(a, b, lighter)
+}
+
+func Overlay(a, b image.Image) image.Image {
+	return BlendPixels(a, b, overlay)
+}
+
+func SoftLight(a, b image.Image) image.Image {
+	return BlendPixels(a, b, softLight)
+}
+
+func HardLight(a, b image.Image) image.Image {
+	return BlendPixels(a, b, hardLight)
+}
+
+func Difference(a, b image.Image) image.Image {
+	return BlendPixels(a, b, difference)
+}
+
+func Exclusion(a, b image.Image) image.Image {
+	return BlendPixels(a, b, exclusion)
+}
+
+func Addition(a, b image.Image) image.Image {
+	return BlendPixels(a, b, addition)
+}
+
+func Subtraction(a, b image.Image) image.Image {
+	return BlendPixels(a, b, subtraction)
+}
+
+func Hue(a, b image.Image) image.Image {
+	return BlendPixels(a, b, hue)
+}
+
+func Saturation(a, b image.Image) image.Image {
+	return BlendPixels(a, b, saturation)
+}
+
+func Color(a, b image.Image) image.Image {
+	return BlendPixels(a, b, colour)
+}
+
+func Luminosity(a, b image.Image) image.Image {
+	return BlendPixels(a, b, luminosity)
 }
