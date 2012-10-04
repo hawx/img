cbec66c177e751768194a79fafb173ec55362a53
Joshua Hawxwell <m@hawx.me>
2012-10-04 18:19:03 +0100

    Added utils.FlagVisited and utils.Adjuster.
    The FlagVisited function simplifies a lot of logic in some tools.
    
    Adjuster replaces the old floatForFloat type. Also added Adder
    and Multiplier functions to quickly create Adjusters.

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"fmt"
 	"os"
+	"flag"
+
 	"image"
 	"image/png"
 	"image/color"
@@ -176,4 +178,37 @@ func EachPixel(img image.Image, f func(c color.Color) color.Color) image.Image {
 	}
 
 	return o
+}
+
+func FlagVisited(name string, flags flag.FlagSet) bool {
+	didFind := false
+	toFind  := flags.Lookup(name)
+
+	flags.Visit(func (f *flag.Flag) {
+		if f == toFind {
+			didFind = true
+		}
+	})
+
+	return didFind
+}
+
+// Adjuster is a function which takes a floating point value, most likely
+// between 0 and 1, and returns another floating point value, again between 0
+// and 1 (though this is not a requirement).
+type Adjuster func (float64) float64
+
+// Adder returns an Adjuster which adds the value with to the returned Adjusters
+// argument.
+func Adder(with float64) Adjuster {
+	return func(i float64) float64 {
+		return i + with
+	}
+}
+
+// Multiplier returns an Adjuster which multiplies a given value with ratio.
+func Multiplier(ratio float64) Adjuster {
+	return func(i float64) float64 {
+		return i * ratio
+	}
 }
