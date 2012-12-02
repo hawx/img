30c13c6a0c26299e4c7ba876ea1034c3ce44bd72
Joshua Hawxwell <m@hawx.me>
2012-12-02 16:59:22 +0000

    Levels curve now linearly interpolates points.
    There are a lot of bugs, it doesn't like just two values for instance,
    and it sometimes produces unexpected results.
    
    At some point this needs to be replaced with a better solution, but
    that will require some research.

--- levels/curve.go
+++ levels/curve.go
@@ -1,17 +1,24 @@
 package levels
 
 import (
+//	"github.com/hawx/img/utils"
 	"fmt"
 	"strings"
 	"strconv"
 )
+
+const (
+	VALUE_DELIM = ","
+	POINT_DELIM = " "
+)
+
 
 type Point struct {
 	X, Y float64
 }
 
 func (p *Point) String() string {
-	return fmt.Sprintf("%v:%v", p.X, p.Y)
+	return fmt.Sprintf("%v" + VALUE_DELIM + "%v", p.X, p.Y)
 }
 
 func P(x, y float64) *Point {
@@ -25,7 +32,7 @@ type Curve struct {
 func (c *Curve) String() string {
 	s := ""
 	for i, p := range c.Points {
-		if i != 0 { s += ", " }
+		if i != 0 { s += POINT_DELIM }
 		s += p.String()
 	}
 
@@ -34,18 +41,30 @@ func (c *Curve) String() string {
 
 // Value calculates the y-value for the given x-value by approximating a bezier
 // curve through the points the Curve describes.
+//
+// BUG(r): Only uses linear interpolation.
 func (c *Curve) Value(x float64) float64 {
-	// First use a linear connecting curve.
-	// - Given a value x find the two Points either side.
-	// - Now linear interpolate the value
+	// find the points either side of x
+	var left, right *Point
 
-	var left, right Point
-	for i, p := range c.Points {
-		if p.X > x {
-			left = p
-			right = c.Points[i + 1]
+	for i := 0; i < len(c.Points) - 1; i++ {
+		if c.Points[i].X == x * 100.0 {
+			return c.Points[i].X / 100.0
+
+		} else if c.Points[i+1].X == x * 100.0 {
+			return c.Points[i+1].X / 100.0
+
+		} else if c.Points[i].X > x {
+			left = c.Points[i]
+			right = c.Points[i+1]
 		}
 	}
+
+	// find the gradient
+	m := (left.Y - right.Y) / (left.X - right.Y)
+
+	// and finally, find the value
+	return (left.Y / 100.0) + (m * (x - (left.X / 100.0)))
 }
 
 func C(ps [][]float64) *Curve {
@@ -59,12 +78,12 @@ func C(ps [][]float64) *Curve {
 
 
 func ParseCurveString(s string) *Curve {
-	pairs  := strings.Split(s, ":")
+	pairs  := strings.Split(s, POINT_DELIM)
 	points := make([]*Point, len(pairs))
 	conv   := func(v string) (r float64) { r, _ = strconv.ParseFloat(v, 64); return }
 
 	for i, p := range pairs {
-		parts := strings.Split(p, ",")
+		parts := strings.Split(p, VALUE_DELIM)
 		points[i] = P(conv(parts[0]), conv(parts[1]))
 	}
 
