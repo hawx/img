3ca7181411f0896afd60bfb368835be40593ca34
Joshua Hawxwell <m@hawx.me>
2013-04-16 18:57:21 +0100

    Moved color-string parsing out to altcolor/parse.go
    The parsing itself is now done with regular expressions and, and!
    allows you to use spaces inside rgb(...)!

---
+++ altcolor/parse.go
@@ -0,0 +1,75 @@
+package altcolor
+
+import (
+	"image/color"
+	"regexp"
+	"strconv"
+)
+
+var (
+	SHORT_HEX = regexp.MustCompile("#([A-Fa-f0-9])([A-Fa-f0-9])([A-Fa-f0-9])([A-Fa-f0-9])?")
+	LONG_HEX  = regexp.MustCompile("#([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})?")
+
+	RGB       = regexp.MustCompile("rgb\\(\\s*(\\d+)\\s*,\\s*(\\d+)\\s*,\\s*(\\d+)\\s*\\)")
+	RGBA      = regexp.MustCompile("rgba\\(\\s*(\\d+)\\s*,\\s*(\\d+)\\s*,\\s*(\\d+)\\s*,\\s*(\\d+)\\s*\\)")
+)
+
+func intToInt(s string) uint8 {
+	r, _ := strconv.Atoi(s)
+	return uint8(r)
+}
+
+func hexToInt(s string) uint8 {
+	r, _ := strconv.ParseInt(s, 16, 16)
+	return uint8(r)
+}
+
+func Parse(str string) color.Color {
+	_parts := SHORT_HEX.FindAllStringSubmatch(str, 5)
+	if _parts != nil {
+		parts := _parts[0]
+		if parts[4] == "" { parts[4] = "F" }
+		return color.NRGBA{
+			hexToInt(parts[1] + parts[1]),
+			hexToInt(parts[2] + parts[2]),
+			hexToInt(parts[3] + parts[3]),
+			hexToInt(parts[4] + parts[4]),
+		}
+	}
+
+	_parts = LONG_HEX.FindAllStringSubmatch(str, 5)
+	if _parts != nil {
+		parts := _parts[0]
+		if parts[4] == "" { parts[4] = "FF" }
+		return color.NRGBA{
+			hexToInt(parts[1]),
+			hexToInt(parts[2]),
+			hexToInt(parts[3]),
+			hexToInt(parts[4]),
+		}
+	}
+
+	_parts = RGB.FindAllStringSubmatch(str, 4)
+	if _parts != nil {
+		parts := _parts[0]
+		return color.NRGBA{
+			intToInt(parts[1]),
+			intToInt(parts[2]),
+			intToInt(parts[3]),
+			255,
+		}
+	}
+
+	_parts = RGBA.FindAllStringSubmatch(str, 5)
+	if _parts != nil {
+		parts := _parts[0]
+		return color.NRGBA{
+			intToInt(parts[1]),
+			intToInt(parts[2]),
+			intToInt(parts[3]),
+			intToInt(parts[4]),
+		}
+	}
+
+	return nil
+}
--- tint.go
+++ tint.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"github.com/hawx/img/altcolor"
 	"github.com/hawx/img/tint"
 	"github.com/hawx/img/utils"
 	"github.com/hawx/hadfield"
 	"fmt"
 	"image/color"
-	"strconv"
-	"strings"
 	"errors"
 )
 
@@ -31,75 +30,22 @@ func (c *localNRGBA) String() string {
 }
 
 func (c *localNRGBA) Set(value string) error {
-	if strings.HasPrefix(value, "#") {
-		parseHex := func(s string) uint8 {
-			r, _ := strconv.ParseInt(s, 16, 16)
-			return uint8(r)
-		}
+	col := altcolor.Parse(value)
 
-		if len(value) == 4 {
-			r := parseHex(value[1:2] + value[1:2])
-			g := parseHex(value[2:3] + value[2:3])
-			b := parseHex(value[3:4] + value[3:4])
-			a := uint8(255)
-			*c = localNRGBA{r, g, b, a}
-
-		} else if len(value) == 5 {
-			r := parseHex(value[1:2] + value[1:2])
-			g := parseHex(value[2:3] + value[2:3])
-			b := parseHex(value[3:4] + value[3:4])
-			a := parseHex(value[4:5] + value[4:5])
-			*c = localNRGBA{r, g, b, a}
-
-		} else if len(value) == 7 {
-			r := parseHex(value[1:3])
-			g := parseHex(value[3:5])
-			b := parseHex(value[5:7])
-			a := uint8(255)
-			*c = localNRGBA{r, g, b, a}
-
-		} else if len(value) == 9 {
-			r := parseHex(value[1:3])
-			g := parseHex(value[3:5])
-			b := parseHex(value[5:7])
-			a := parseHex(value[7:9])
-			*c = localNRGBA{r, g, b, a}
-
-		} else {
-			return errors.New(`unknown hexadecimal format. Accepts:
-  #FFF                  3-digit hexadecimal (#RGB)
-  #FFFC                 4-digit hexadecimal (#RGBA)
-  #F0CDBB               6-digit hexadecimal (#RRGGBB)
-  #F0CDBBAC             8-digit hexadecimal (#RRGGBBAA)
-`)
-		}
-
-	} else if strings.HasPrefix(value, "rgb(") {
-		parts := strings.Split(value[4:len(value)-1], ",")
-		r, _ := strconv.Atoi(parts[0])
-		g, _ := strconv.Atoi(parts[1])
-		b, _ := strconv.Atoi(parts[2])
-		*c = localNRGBA{uint8(r), uint8(g), uint8(b), 255}
-
-	} else if strings.HasPrefix(value, "rgba(") {
-		parts := strings.Split(value[5:len(value)-1], ",")
-		r, _ := strconv.Atoi(parts[0])
-		g, _ := strconv.Atoi(parts[1])
-		b, _ := strconv.Atoi(parts[2])
-		a, _ := strconv.ParseFloat(parts[3], 32)
-		*c = localNRGBA{uint8(r), uint8(g), uint8(b), uint8(a * 255)}
-
-	} else {
+	if col == nil {
 		return errors.New(`unknown string format. Accepts:
   #FFF                  3-digit hexadecimal (#RGB)
   #FFFC                 4-digit hexadecimal (#RGBA)
   #F0CDBB               6-digit hexadecimal (#RRGGBB)
   #F0CDBBAC             8-digit hexadecimal (#RRGGBBAA)
 
-  rgb(100,50,10)        Red, green, blue (must not contain spaces)
-  rgba(100,50,10,.5)    Red, green, blue and alpha (must not contain spaces)
+  rgb(100,50,10)        Red, green, blue
+  rgba(100,50,10,127)    Red, green, blue and alpha
 `)
 	}
+
+	r,g,b,a := utils.NormalisedRGBA(col)
+	*c = localNRGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
 
 	return nil
 }
